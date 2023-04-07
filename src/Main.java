92ae81ceadba88ba43edc08673527f8faf9b57b6
Acer Home <klimrayko@gmail.com>
2023-04-07 09:42:46 +0600

    This is the fibonacci calculator

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,14 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
-
-        System.out.println(factorial(n));
-
-
-
-
+        System.out.println(Fibonacci(n));
     }
-    public static int factorial(int n){
-        if(n==0){
-            return 1;
-        } else if (n==1) {
-            return 1;
-        }
-        return n*factorial(n-1);
+    public static int Fibonacci(int n) {
+        if(n==1 || n==0) return n;
+        return Fibonacci(n-1) + Fibonacci(n-2);
     }
+
 
 }
