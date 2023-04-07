37484370e954539d748cfe55df635b1e3baf87c1
Acer Home <klimrayko@gmail.com>
2023-04-07 09:40:33 +0600

    This is the prime checker

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,31 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int a = input.nextInt();
         int n = input.nextInt();
-        System.out.println(power(a,n));
-    }
-    public static int power(int a, int n){
-        if(a==0 || a == 1 || n == 1 ){
-            return a;
-        } else if (n == 0) {
-            return 1;
+        int i =(int)Math.sqrt(n);
+        if(isPrime(n,i)){
+            System.out.println("YES");
         }
-        return a*power(a,n-1);
+        else {
+            System.out.println("NO");
+        }
+
+
+
+
+
+
+    }
+    public static boolean isPrime(int n, int i){
+        if(i <= 1 ){
+            return true;
+
+        } else if (n%i==0) {
+            return false;
+        }
+        return isPrime(n,i-1);
     }
 
 }
