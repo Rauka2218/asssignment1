f8005358f3b3d64648a84e72d7ecada46cc62b40
Acer Home <klimrayko@gmail.com>
2023-04-09 22:49:56 +0600

    This is the array reverser.

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,24 @@
-import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int n = input.nextInt();
-        System.out.println(Fibonacci(n));
-    }
-    public static int Fibonacci(int n) {
-        if(n==1 || n==0) return n;
-        return Fibonacci(n-1) + Fibonacci(n-2);
+        int[] arr = {1, 2, 3, 4, 5};
+        reverse(arr);
+        System.out.println("Reversed array: ");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
     }
 
+    public static void reverse(int[] arr) {
+        reverseHelper(arr, 0, arr.length - 1);
+    }
 
+    public static void reverseHelper(int[] arr, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        reverseHelper(arr, start + 1, end - 1);
+    }
 }
